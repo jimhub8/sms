5bf6a09f2941af56b9a18ec906186b9293ff13a5
jimhub8 <51815444+jimhub8@users.noreply.github.com>
2022-03-21 16:40:11 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::post('filter_messages', 'MessagesController@filter_messages')->name('filt
 // })->name('dashboard');
 
 Route::post('/waybill', 'HomeController@waybill')->name('waybill');
-Route::get('/track', 'HomeController@track')->name('track');
+// Route::get('/track', 'HomeController@track')->name('track');
 
 
 Route::group(['middleware' => ['auth']], function () {
